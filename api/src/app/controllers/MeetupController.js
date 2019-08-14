75ddba8793ee08022dba56f756fe41a92ec4991d
Daniel Lucas <dlsbdaniel@gmail.com>
2019-08-14 00:37:10 -0300

    Adding Meetup model, controller and migration

---
+++ api/src/app/controllers/MeetupController.js
@@ -0,0 +1,43 @@
+import * as Yup from 'yup';
+import { isBefore, parseISO } from 'date-fns';
+
+import Meetup from '../models/Meetup';
+
+class MeetupController {
+  async index(req, res, next) {}
+
+  async store(req, res, next) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+      file_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
+    if (isBefore(parseISO(req.body.date), new Date())) {
+      return res
+        .status(400)
+        .json({ error: 'You cannot create a meetup in the past' });
+    }
+
+    const user_id = req.userId;
+
+    const meetup = await Meetup.create({
+      ...req.body,
+      user_id,
+    });
+
+    return res.json(meetup);
+  }
+
+  async update(req, res, next) {}
+
+  async delete(req, res, next) {}
+}
+
+export default new MeetupController();
--- api/src/routes.js
+++ api/src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
+import MeetupController from './app/controllers/MeetupController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -15,6 +16,11 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+routes.get('/meetups', MeetupController.index);
+routes.post('/meetups', MeetupController.store);
+routes.put('/meetups/:id', MeetupController.update);
+routes.delete('/meetups/:id', MeetupController.delete);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
