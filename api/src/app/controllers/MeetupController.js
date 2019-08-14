180248b8ad92ee3216d8f4815bb176f9499af08b
Daniel Lucas <dlsbdaniel@gmail.com>
2019-08-14 04:05:35 -0300

    Adding the remaining functions of MeetupController

--- api/src/app/controllers/MeetupController.js
+++ api/src/app/controllers/MeetupController.js
@@ -2,6 +2,7 @@ import * as Yup from 'yup';
 import { isBefore, parseISO } from 'date-fns';
 
 import Meetup from '../models/Meetup';
+import File from '../models/File';
 
 class MeetupController {
   async index(req, res, next) {}
@@ -15,10 +16,16 @@ class MeetupController {
       file_id: Yup.number().required(),
     });
 
+    /**
+     * Yup validation
+     */
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation Fails' });
     }
 
+    /**
+     * Verifying if user is trying to create a meetup in the past
+     */
     if (isBefore(parseISO(req.body.date), new Date())) {
       return res
         .status(400)
@@ -35,9 +42,103 @@ class MeetupController {
     return res.json(meetup);
   }
 
-  async update(req, res, next) {}
+  async update(req, res, next) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+      file_id: Yup.number().required(),
+    });
 
-  async delete(req, res, next) {}
+    /**
+     * Yup validation
+     */
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
+    const meetup_id = req.params.id;
+
+    const meetup = await Meetup.findByPk(meetup_id, {
+      include: [
+        {
+          model: File,
+          attributes: ['name', 'path'],
+        },
+      ],
+    });
+
+    /**
+     * If the meetup doesn't exist
+     */
+    if (!meetup) {
+      return res.status(500).json({ error: 'That meetup does not exist' });
+    }
+
+    /**
+     * If the meetup already happened
+     */
+    if (isBefore(meetup.date, new Date())) {
+      return res
+        .status(400)
+        .json({ error: 'You cannot modify a meetup that already happened' });
+    }
+
+    /**
+     * If the user is trying to update a meetup to happen in the past
+     */
+    if (isBefore(parseISO(req.body.date), new Date())) {
+      return res
+        .status(400)
+        .json({ error: 'You cannot update a meetup to happen in the past' });
+    }
+
+    /**
+     * Verifying if the user owns the passed meetup
+     */
+    if (meetup.user_id !== req.userId) {
+      return res
+        .status(400)
+        .json({ error: "You don't have permission to update this meetup" });
+    }
+
+    await meetup.update(req.body);
+
+    return res.json(meetup);
+  }
+
+  async delete(req, res, next) {
+    const meetup_id = req.params.id;
+
+    const meetup = await Meetup.findByPk(meetup_id);
+
+    if (!meetup) {
+      return res.status(500).json({ error: 'That meetup does not exist' });
+    }
+
+    /**
+     * Verifying if the meetup already happened
+     */
+    if (isBefore(meetup.date, new Date())) {
+      return res
+        .status(400)
+        .json({ error: 'You cannot delete a meetup that already happened' });
+    }
+
+    /**
+     * Verifying if the user owns the passed meetup
+     */
+    if (meetup.user_id !== req.userId) {
+      return res
+        .status(400)
+        .json({ error: "You don't have permission to delete this meetup" });
+    }
+
+    await meetup.destroy();
+
+    return res.json({ message: 'Meetup succesfully deleted' });
+  }
 }
 
 export default new MeetupController();
